ff90d46b68b98bc912b7c203c9d59acaa4991e19
Gabriela Sewdhan <gabiems13@gmail.com>
2021-01-29 15:48:24 -0400

    Added AJAX Functionality

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, url_for, jsonify
 import pymongo
 from flask_pymongo import PyMongo
-from libs.constants import MONGODB
+# from libs.constants import MONGODB
 
 mongo = PyMongo()
 app = Flask(__name__)
